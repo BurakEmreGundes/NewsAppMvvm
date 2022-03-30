87649b0bc0e1716e8f707864e62a23c9a00c2154
BurakEmreGundes <burak.gundes@ogr.sakarya.edu.tr>
2022-03-31 00:40:19 +0300

    ViewModel Oluşturuldu
    ViewModel katmanı oluşturuldu. Article viewmodel düzenlendi. Cell tasarımı yapıldı.

---
+++ NewsAppMvvm/NewsAppMvvm/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTableViewCell.swift
+//  NewsAppMvvm
+//
+//  Created by Burak Emre gündeş on 31.03.2022.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell : UITableViewCell{
+    
+    @IBOutlet weak var titleLabel : UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
